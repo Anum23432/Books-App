9647556b273951954ffb21129b98cd6c7cd16fec
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-10-28 14:39:26 +0500

    booklist app

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,3 @@
-import {useHistory} from "react-router-dom";
 import React, {useEffect, useState} from 'react';
 // import {Card,Row,Col} from 'react-bootstrap/Card'
 
@@ -6,7 +5,7 @@ import React, {useEffect, useState} from 'react';
 
 function BookList() {
    
-   let history = useHistory();
+   
 
    const [bookDetails, setBookDetails] = useState([]);
    useEffect(() => {
@@ -22,14 +21,14 @@ return (
 
    
 <>
-<button style={{marginLeft:'43%',boxShadow:'0 0 10px 0 black'}} className="btn btn-success"onClick = {() => history.push('/')}>Back to Homepage</button><br/>
+
 
 {bookDetails.map((book) =>(
 <div className="red"> 
-<div className="card" style={{width:'18rem',boxShadow:'0 0 10px 0 rgb(30, 109, 65)',borderRadius:'10px'}}>
+<div className="card" style={{width:'16rem',boxShadow:'0 0 10px 0 rgb(30, 109, 65)',borderRadius:'6px'}}>
 <h4>Title : {book.title}</h4>   
 <h5>Pages : {book.pages}</h5>
-<h5>Release Date {book.releaseDate}</h5>
+<h5>Release Date : {book.releaseDate}</h5>
 <h5>ISBN : {book.isbn}</h5>
 </div>
 </div>
